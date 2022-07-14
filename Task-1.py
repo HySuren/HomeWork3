5c6491a8f4db6739567e55b3db58cfcfa6c61df6
HySuren <104077284+HySuren@users.noreply.github.com>
2022-07-14 15:33:13 +0300

    Update Task-1.py

--- Task-1.py
+++ Task-1.py
@@ -1,8 +1,9 @@
 import time
 
 
-def time_check(function,call_count= 0,start_sleep_time= 1,factor= 2,border_cleep_time=60):
+def time_check(function,call_count= 3,start_sleep_time= 1,factor= 2,border_cleep_time=60):
     call_count = call_count
+    call_count_break = 0
     start_sleep_time = start_sleep_time
     factor = factor
     border_sleep_time = border_cleep_time
@@ -13,24 +14,22 @@ def time_check(function,call_count= 0,start_sleep_time= 1,factor= 2,border_cleep
         nonlocal t
         nonlocal factor
         nonlocal border_sleep_time
+        nonlocal call_count_break
         result = function(*args, **kwargs)
-        if function:
-            call_count += 1
-            if t < border_sleep_time:
-                t = start_sleep_time*factor**t
-                timer = time.sleep(t)
-            if t >= border_sleep_time:
-                t = border_sleep_time
-                timer = time.sleep(border_sleep_time)
-        return f"Кол-во запусков = {call_count}\nНачало работы\nЗапуск номер {call_count}. Ожидание: {t} секунд. Результат декорируемой функций = {result}"
+        if t < border_sleep_time:
+            t *= factor
+            timer = time.sleep(t)
+        if t >= border_sleep_time:
+            t = border_sleep_time
+            timer = time.sleep(border_sleep_time)
+        call_count_break += 1
+        print(f"Кол-во запусков = {call_count_break}\nНачало работы\nЗапуск номер {call_count_break}. Ожидание: {t} секунд. Результат декорируемой функций = {result}")
+        while call_count_break != call_count:
+            sleep_time(function.__call__(*args))
     return sleep_time
-
 
 @time_check
 def list_append(args: int):
     return args**2
 
-
-print(list_append(22))
-print(list_append(8))
-print(list_append(7))
+print(list_append(5))
