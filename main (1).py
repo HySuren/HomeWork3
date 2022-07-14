34e847bcf7df30d6134e01e42874ba6058030a27
HySuren <104077284+HySuren@users.noreply.github.com>
2022-07-14 16:22:08 +0300

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,17 +1,17 @@
 from typing import Union
-from heroes import Superman, SuperHero
+from heroes import Superman, SuperHero, News
 from places import Kostroma, Tokyo
 
 
-def save_the_place(hero: SuperHero, place: Union[Kostroma, Tokyo]):
+def save_the_place(hero: SuperHero,place: Union[Kostroma, Tokyo]):
     hero.find(place)
     hero.attack()
     if hero.can_use_ultimate_attack:
         hero.ultimate()
-    hero.create_news(place)
+    News.create_news(place,hero)
 
 
 if __name__ == '__main__':
-    save_the_place(Superman(), Kostroma())
+    save_the_place(Superman(),Kostroma())
     print('-' * 20)
     save_the_place(SuperHero('Chack Norris', False), Tokyo())
